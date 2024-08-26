20c1da1ad736729009276b93e1696562b8ea37c7
atishe <you@example.com>
2024-08-26 15:07:55 +0530

    backend update

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: process.env.NODE_ENV === "production"
-        ? ""
-        : "http://localhost:5173",
+    origin: true,
     credentials: true
 }))
 
